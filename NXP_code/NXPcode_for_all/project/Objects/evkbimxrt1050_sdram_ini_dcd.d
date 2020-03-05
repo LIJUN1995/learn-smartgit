c7b0d65e32ee8cf8421cdeb5f2e9afb1b1b1d9e8
lijun <lijun@cdfinger.com>
2020-03-05 15:10:07 +0800

    删除MCU相关代码

--- NXP_code/NXPcode_for_all/project/Objects/evkbimxrt1050_sdram_ini_dcd.d
+++
@@ -1,3 +0,0 @@
-.\objects\evkbimxrt1050_sdram_ini_dcd.o: ..\nxplib\xip\evkbimxrt1050_sdram_ini_dcd.c
-.\objects\evkbimxrt1050_sdram_ini_dcd.o: ..\nxplib\xip\evkbimxrt1050_sdram_ini_dcd.h
-.\objects\evkbimxrt1050_sdram_ini_dcd.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- NXP_code/NXPcode_for_all/project/Objects/startup_mimxrt1052.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_mimxrt1052.o: ..\nxplib\cmsis\startup_MIMXRT1052.s
