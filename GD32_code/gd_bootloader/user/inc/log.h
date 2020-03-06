0f738e790c2c19827c3eabe3a115b5c46aeb3993
lijun <lijun@cdfinger.com>
2020-03-06 09:49:15 +0800

    删除MCU代码

--- GD32_code/gd_bootloader/user/inc/log.h
+++
@@ -1,7 +0,0 @@
-#ifndef LOG_H_
-#define LOG_H_
-
-#define SPI_TRANSFER
-//#define QUARTERPICTURE
-//#define NAVI_MATRESIZE
-#endif
--- NXP_code/NXPcode_for_all/project/Objects/libalgorithm_fpopticv_1.01.07_256×256_64bit.d
+++
@@ -1,1 +0,0 @@
-.\objects\libalgorithm_fpopticv_1.01.07_256¡Á256_64bit.o: C:\Users\sunlin\Desktop\libalgorithm_fpOpticV_1.01.07_256¡Á256_64bit\libalgorithm_fpOpticV_1.01.07_256¡Á256_64bit.a
